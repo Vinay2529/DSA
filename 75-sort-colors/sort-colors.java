961921c173755566444933faaf1d99884830b550
Vinay2529 <123531815+Vinay2529@users.noreply.github.com>
2025-12-23 11:33:04 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (15.54%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,37 +1,27 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int n = nums.length;
-        int max = maximum(nums, n);
-
-        int[] temp = new int[max + 1];
-
-        for (int i = 0; i < n; i++) {
-            temp[nums[i]]++;
-        }
-
-        for (int i = 1; i < temp.length; i++) {
-            temp[i] = temp[i] + temp[i - 1];
-        }
-
-        int[] count = new int[n];
-
-        for (int i = n - 1; i >= 0; i--) {
-            int pos = temp[nums[i]] - 1;
-            count[pos] = nums[i];
-            temp[nums[i]]--;
-        }
-        for (int i = 0; i < n; i++) {
-            nums[i] = count[i];
-        }
-    }
-
-    private int maximum(int[] nums, int n) {
-        int a = nums[0];
-        for (int i = 1; i < n; i++) {
-            if (nums[i] > a) {
-                a = nums[i];
+        int i=0;
+        int left=0,right=nums.length-1;
+        while(i<=right)
+        {
+            if(nums[i]==0)
+            {
+                int temp=nums[i];
+                nums[i]=nums[left];
+                nums[left]=temp;
+                left++;
+                i++;
+            }
+            else if(nums[i]==2)
+            {
+                int temp=nums[i];
+                nums[i]=nums[right];
+                nums[right]=temp;
+                right--;
+            }
+            else {
+                i++;
             }
         }
-        return a;
     }
 }
