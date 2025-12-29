770028a90b46604093474fff70eee776b38f38b3
Vinay2529 <123531815+Vinay2529@users.noreply.github.com>
2025-12-29 21:41:53 +0530

    Time: 3 ms (20.32%) | Memory: 52.9 MB (12.09%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -1,55 +1,42 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        //find intersection OF TWO NODES
-        ListNode fp = headA;
-        ListNode sp = headB;
-        int m = length(headA);
-        int n = length(headB);
-        if (m < n) {
-            while (m != n) {
-                sp = sp.next;
-                n--;
+        int x=getlen(headA);
+        int y=getlen(headB);
+        if(x<y)
+        {
+            while(y!=x)
+            {
+                headB=headB.next;
+                y--;
             }
-        } else {
-            while (m != n) {
-                fp = fp.next;
-                m--;
-            }
-
         }
-        return Intersect(fp, sp);
-    }
-
-    private int length(ListNode head) {
-        int count = 0;
-        ListNode curr = head;
-        while (curr != null) {
-            count++;
-            curr = curr.next;
-        }
-        return count;
-    }
-
-    private ListNode Intersect(ListNode fp, ListNode sp) {
-        while (fp != null || sp != null) {
-            if (fp == sp) {
-                return fp;
-            } else {
-                fp = fp.next;
-                sp = sp.next;
+        else{
+            while(x!=y)
+            {
+                headA=headA.next;
+                x--;
             }
+        }
+        while(headA!=null && headB!=null)
+        {
+            if(headA==headB)
+            {
+                return headA;
+            }
+            headA=headA.next;
+            headB=headB.next;
         }
         return null;
+    }
+    private int getlen(ListNode head)
+    {
+        int count=0;
+        ListNode curr=head;
+        while(curr!=null)
+        {
+            count++;
+            curr=curr.next;
+        }
+        return count;
     }
 }
