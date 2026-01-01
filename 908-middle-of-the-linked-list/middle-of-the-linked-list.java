452d4b3db2cc861eee4ded3698036f756e90e7f7
Vinay2529 <123531815+Vinay2529@users.noreply.github.com>
2026-01-01 10:35:52 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (92.73%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.java
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -16,13 +16,9 @@ class Solution {
         }
         int n=findlen(head);
         int start=n/2;
-        // if(n%2!=0)
-        // {
-        //     start=(n/2)+1;
-        // }
         ListNode curr=head;
         int z=0;
-        while(z<start && curr.next!=null)
+        while(z<start)
         {
             curr=curr.next;
             z++;
