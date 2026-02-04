81ff6482deb1acea518ed87708a41c848079582a
Vinay2529 <123531815+Vinay2529@users.noreply.github.com>
2026-02-04 16:13:46 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (98.63%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,25 +1,40 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int start = 0, end = nums.length - 1;
+        int por = findpor(nums);
 
+        if (por == 0 || target < nums[0]) {
+            return binary(nums, por, nums.length - 1, target);
+        } else {
+            return binary(nums, 0, por - 1, target);
+        }
+    }
+
+    private int binary(int[] nums, int start, int end, int key) {
         while (start <= end) {
             int mid = (start + end) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[start] <= nums[mid]) {
-                if (target >= nums[start] && target < nums[mid]) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
-                }
+            if (nums[mid] < key) {
+                start=mid+1;
+            } else if (nums[mid] > key) {
+                end=mid-1;
             } else {
-                if (target > nums[mid] && target <= nums[end]) {
-                    start = mid + 1;
-                } else {
-                    end = mid - 1;
-                }
+                return mid;
             }
         }
         return -1;
+    }
+
+    private int findpor(int[] nums) {
+        int start = 0, end = nums.length - 1;
+        int ans = 0;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] < nums[0]) {
+                ans = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return ans;
     }
 }
