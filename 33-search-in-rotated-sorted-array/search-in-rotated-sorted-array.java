1319ad7af161be105c4b84777428c524dd0cf7e7
Vinay2529 <123531815+Vinay2529@users.noreply.github.com>
2026-02-06 21:38:59 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (79.41%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,38 +1,46 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int por = findpor(nums);
-
-        if (por == 0 || target < nums[0]) {
-            return binary(nums, por, nums.length - 1, target);
-        } else {
-            return binary(nums, 0, por - 1, target);
+        int por=pointOfRotation(nums);
+        if(por==0 || target<nums[0])
+        {
+            return binarySearch(nums,por,nums.length-1,target);
+        }
+        else{
+            return binarySearch(nums,0,por-1,target);
         }
     }
-
-    private int binary(int[] nums, int start, int end, int key) {
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            if (nums[mid] < key) {
+    private int binarySearch(int[] nums,int start,int end,int key)
+    {
+        while(start<=end)
+        {
+            int mid=(start+end)/2;
+            if(nums[mid]<key)
+            {
                 start=mid+1;
-            } else if (nums[mid] > key) {
+            }
+            else if(nums[mid]>key)
+            {
                 end=mid-1;
-            } else {
+            }
+            else{
                 return mid;
             }
         }
         return -1;
     }
-
-    private int findpor(int[] nums) {
-        int start = 0, end = nums.length - 1;
-        int ans = 0;
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            if (nums[mid] < nums[0]) {
-                ans = mid;
-                end = mid - 1;
-            } else {
-                start = mid + 1;
+    private int pointOfRotation(int[] nums)
+    {
+        int ans=0;
+        int start=0,end=nums.length-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(nums[mid]<nums[0])
+            {
+                ans=mid;
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
             }
         }
         return ans;
