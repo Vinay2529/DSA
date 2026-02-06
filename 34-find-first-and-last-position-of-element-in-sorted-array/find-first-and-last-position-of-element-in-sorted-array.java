2287ee5083662987cfa29d0e3e7132f055c0b760
Vinay2529 <123531815+Vinay2529@users.noreply.github.com>
2026-02-06 15:00:58 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (67.54%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,21 +1,20 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int first=findfirst(nums,target);
+        int first=findFirst(nums,target);
         if(first==-1) return new int[]{-1,-1};
-        int last=findlast(nums,target);
+        int last=findLast(nums,target);
         return new int[]{first,last};
     }
-    private int findfirst(int[] nums,int target)
-    {
+    private int findFirst(int[] nums,int target){
         int start=0,end=nums.length-1;
         int ans=-1;
         while(start<=end){
-            int mid=start+(end-start)/2;
-            if(nums[mid]>target){
-                end=mid-1;
-            }
-            else if(nums[mid]<target){
+            int mid=(start+end)/2;
+            if(nums[mid]<target){
                 start=mid+1;
+            }
+            else if(nums[mid]>target){
+                end=mid-1;
             }
             else{
                 ans=mid;
@@ -24,17 +23,16 @@ class Solution {
         }
         return ans;
     }
-    private int findlast(int[] nums,int target)
-    {
+    private int findLast(int[] nums,int target){
         int start=0,end=nums.length-1;
         int ans=-1;
         while(start<=end){
-            int mid=start+(end-start)/2;
-            if(nums[mid]>target){
-                end=mid-1;
-            }
-            else if(nums[mid]<target){
+            int mid=(start+end)/2;
+            if(nums[mid]<target){
                 start=mid+1;
+            }
+            else if(nums[mid]>target){
+                end=mid-1;
             }
             else{
                 ans=mid;
