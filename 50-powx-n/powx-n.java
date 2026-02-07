c49293906a8e3e18867235d2d7d7aab3cdaf9d8c
Vinay2529 <123531815+Vinay2529@users.noreply.github.com>
2026-02-07 15:09:10 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (45.78%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,21 +1,16 @@
 class Solution {
     public double myPow(double x, int n) {
-        if(n==0)
-        {
+        if (n == 0) {
             return 1;
-        }
-        else{
-            double res=myPow(x,n/2);
-            if(n%2==0)
-            {
-                return res*res;
-            }
-            else{
-                if(n>0){
-                    return res*res*x;
-                }
-                else{
-                    return (res*res)/x;
+        } else {
+            double res = myPow(x, n / 2);
+            if (n % 2 == 0) {
+                return res * res;
+            } else {
+                if (n > 0) {
+                    return res * res * x;
+                } else {
+                    return (res * res) / x;
                 }
             }
         }
