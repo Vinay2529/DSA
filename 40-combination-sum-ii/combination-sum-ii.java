e2a858ce471ef6988af29728c253d99c5c5dc712
Vinay2529 <123531815+Vinay2529@users.noreply.github.com>
2026-02-17 16:25:29 +0530

    Time: 5 ms (99.22%) | Memory: 45.7 MB (15.84%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,30 +1,33 @@
 class Solution {
     public List<List<Integer>> answer = new ArrayList<>();
-
+    public int[] candidates;
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
-        function(0, target, new ArrayList<>(), candidates);
+        this.candidates=candidates;
+        backtrack(0,target,new ArrayList<>());
         return answer;
     }
 
-    private void function(int start, int rem, List<Integer> combination, int[] candidates) {
-
-        if (rem == 0) {
+    private void backtrack(int start, int target, List<Integer> combination) {
+        if(target==0)
+        {
             answer.add(new ArrayList<>(combination));
             return;
         }
-
-        else {
-            for (int i = start; i < candidates.length; i++) {
-                if (i > start && candidates[i] == candidates[i - 1]) {
+        else{
+            for(int i=start;i<candidates.length;i++)
+            {
+                if(i>start && candidates[i]==candidates[i-1])
+                {
                     continue;
                 }
-                if (candidates[i] > rem) {
+                if(candidates[i]>target)
+                {
                     break;
                 }
                 combination.add(candidates[i]);
-                function(i + 1, rem - candidates[i], combination, candidates);
-                combination.remove(combination.size() - 1);
+                backtrack(i+1,target-candidates[i],combination);
+                combination.remove(combination.size()-1);
             }
         }
     }
