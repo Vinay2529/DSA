13324064db4bccf23b31df0c24b143d28708a975
Vinay2529 <123531815+Vinay2529@users.noreply.github.com>
2026-02-21 15:11:58 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (76.20%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,29 +1,22 @@
 class Solution {
-    public List<List<Integer>> subsets;
+    public List<List<Integer>> answer;
     public int[] nums;
-
     public List<List<Integer>> subsets(int[] nums) {
-        subsets=new ArrayList<>();
+        answer=new ArrayList<>();
         this.nums=nums;
-
-        List<Integer> subset=new ArrayList<>();
-        backtrack(0,subset);
-        return this.subsets;
+        function(new ArrayList<>(),0);
+        return answer;
     }
-    private void backtrack(int i,List<Integer> subset){
-        if(i==nums.length){
-            // List<Integer> ans=new ArrayList<>();
-            // for(int num:subset)
-            // {
-            //     ans.add(num);
-            // }
-            // subsets.add(ans);
-            subsets.add(new ArrayList<>(subset));
+    private void function(List<Integer> subset,int i)
+    {
+        if(i==nums.length)
+        {
+            answer.add(new ArrayList<>(subset));
         }
         else{
-            backtrack(i+1,subset);
+            function(subset,i+1);
             subset.add(nums[i]);
-            backtrack(i+1,subset);
+            function(subset,i+1);
             subset.remove(subset.size()-1);
         }
     }
