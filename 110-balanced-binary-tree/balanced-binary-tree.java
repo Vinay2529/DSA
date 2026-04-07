cdc888a6d18f8ec3a2e5668dcb5947e15b56040b
Vinay2529 <123531815+Vinay2529@users.noreply.github.com>
2026-04-07 22:01:08 +0530

    Time: 1 ms (25.64%) | Memory: 45.4 MB (93.48%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -24,14 +24,14 @@ class Solution {
         }
         return isBalanced(root.left) && isBalanced(root.right);
     }
-
-    private int height(TreeNode node) {
-        if (node == null)
+    private int height(TreeNode root){
+        if(root==null){
             return 0;
+        }
+        int left=height(root.left);
+        int right=height(root.right);
 
-        int left = height(node.left);
-        int right = height(node.right);
-
-        return Math.max(left, right) + 1;
-    }
+       return Math.max(left,right)+1; 
+    }    
+    
 }
