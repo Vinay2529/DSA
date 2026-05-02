49eeedf267b2502b9a21cbc5913f20aed736da15
Vinay2529 <123531815+Vinay2529@users.noreply.github.com>
2026-05-02 13:49:23 +0530

    Time: 14 ms (71.25%) | Memory: 47.9 MB (19.48%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -5,20 +5,18 @@ class Solution {
             mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
         }
         PriorityQueue<Map.Entry<Integer,Integer>> minHeap=new PriorityQueue<>(
-            (a,b) -> a.getValue() - b.getValue()
+            (a,b)->a.getValue()-b.getValue()
         );
-
-        for (Map.Entry<Integer,Integer> entry:mp.entrySet()) {
+        for(Map.Entry<Integer,Integer> entry:mp.entrySet()){
             minHeap.offer(entry);
-
-            if (minHeap.size() > k) {
+            if(minHeap.size()>k){
                 minHeap.poll();
             }
         }
         int[] arr=new int[k];
-        int i=0;
-        while(!minHeap.isEmpty()){
-            arr[i++]=minHeap.poll().getKey();
+        int j=0;
+        for(int i=0;i<k;i++){
+            arr[j++]=minHeap.poll().getKey();
         }
         return arr;
     }
