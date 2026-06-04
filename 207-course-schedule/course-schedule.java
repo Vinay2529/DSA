8370f2e631d1bee35504f127765f69f1ae0a7b81
Vinay2529 <123531815+Vinay2529@users.noreply.github.com>
2026-06-04 15:35:02 +0530

    Time: 8 ms (25.46%) | Memory: 47.2 MB (46.14%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,39 @@
+class Solution {
+    private List<List<Integer>> adj=new ArrayList<>();
+    private Map<Integer,String> state=new HashMap<>();
+    private boolean isCycleFound=false;
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        for(int i=0;i<numCourses;i++){
+            adj.add(new ArrayList<>());
+            state.put(i,"U");
+        }
+        for(int i=0;i<prerequisites.length;i++){
+            int[] req=prerequisites[i];
+            int fromNode=req[1];
+            int toNode=req[0];
+            adj.get(fromNode).add(toNode);
+        }
+        for(int i=0;i<numCourses;i++){
+            if(state.get(i).equals("U")){
+                dfs(i);
+            }
+            if(isCycleFound==true){
+                return false;
+            }
+        }
+        return !isCycleFound;
+    }
+    public void dfs(int node){
+        if(state.get(node).equals("U")){
+            state.put(node,"V");
+            List<Integer> neighbour=adj.get(node);
+            for(int nei:neighbour){
+                dfs(nei);
+            }
+            state.put(node,"P");
+        }
+        else if(state.get(node).equals("V")){
+            isCycleFound=true;
+        }
+    }
+}
