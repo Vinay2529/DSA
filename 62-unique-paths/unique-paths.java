773600e36455e355ec2c65e8db86d4d1a85dbe03
Vinay2529 <123531815+Vinay2529@users.noreply.github.com>
2026-06-08 14:39:50 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (99.26%) - LeetSync

---
+++ 62-unique-paths/unique-paths.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp=new int[m][n];
+        for(int[] row:dp) Arrays.fill(row,-1);
+        return recur(m-1,n-1,dp);
+    }
+    private int recur(int i,int j,int[][] dp){
+        if(i==0 && j==0) return 1;
+        if(i<0 || j<0) return 0;
+
+        if(dp[i][j]!= -1) return dp[i][j];
+
+        dp[i][j]=recur(i-1,j,dp)+recur(i,j-1,dp);
+        return dp[i][j];
+    }
+}
