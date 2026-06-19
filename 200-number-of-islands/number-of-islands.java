34038ab8155f364d62ff7ab9bc46b3452de0f37d
Vinay2529 <123531815+Vinay2529@users.noreply.github.com>
2026-06-19 12:11:54 +0530

    Time: 3 ms (87.49%) | Memory: 52.3 MB (46.40%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,33 +1,32 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int count = 0;
-        for(int row = 0; row < grid.length; row++) {
-            for(int col = 0; col < grid[0].length; col++) {
-                if (grid[row][col] == '1') {
+        int count=0;
+        int row=grid.length,col=grid[0].length;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(grid[i][j]=='1'){
                     count++;
-                    dfs(row, col, grid);
+                    dfs(i,j,grid);
                 }
             }
         }
-
         return count;
     }
-
-    public void dfs(int row, int col, char[][] grid) {
-        if (isValid(row, col, grid)) {
-            grid[row][col] = '2';
-            dfs(row - 1, col, grid);
-            dfs(row + 1, col, grid);
-            dfs(row, col - 1, grid);
-            dfs(row, col + 1, grid);
+    private void dfs(int row,int col,char[][] grid){
+        if(isValid(row,col,grid)){
+            grid[row][col]='2';
+            dfs(row+1,col,grid);
+            dfs(row-1,col,grid);
+            dfs(row,col+1,grid);
+            dfs(row,col-1,grid);
         }
     }
 
-    public boolean isValid(int row, int col, char[][] grid) {
-        if (row < 0 || row >= grid.length) return false;
-        if (col < 0 || col >= grid[0].length) return false;
+    private boolean isValid(int row,int col,char[][] grid){
+        if(row<0 || row>=grid.length) return false;
+        if(col<0 || col>=grid[0].length) return false;
 
-        if (grid[row][col] == '1') return true;
+        if(grid[row][col]=='1') return true;
         return false;
     }
 }
