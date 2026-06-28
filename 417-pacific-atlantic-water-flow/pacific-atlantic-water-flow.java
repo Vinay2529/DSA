a67067a48ef7796a6291e72326ef447ff47ae554
Vinay2529 <123531815+Vinay2529@users.noreply.github.com>
2026-06-28 18:30:57 +0530

    Time: 5 ms (56.53%) | Memory: 47.5 MB (58.49%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -1,27 +1,25 @@
 class Solution {
     public List<List<Integer>> pacificAtlantic(int[][] height) {
-        List<List<Integer>> result = new ArrayList<>();
-        if (height == null || height.length == 0 || height[0].length == 0) {
+        List<List<Integer>> result=new ArrayList<>();
+        if(height==null || height.length==0 || height[0].length==0){
             return result;
         }
-        int m = height.length;
-        int n = height[0].length;
-        boolean[][] pacific = new boolean[m][n];
-        boolean[][] atlantic = new boolean[m][n];
-
-        for (int j = 0; j < n; j++) {
-            dfs(height, pacific, 0, j);
-            dfs(height, atlantic, m - 1, j);
+        int m=height.length;
+        int n=height[0].length;
+        boolean[][] CanReachAtlantic=new boolean[m][n];
+        boolean[][] CanReachPacific=new boolean[m][n];
+        for(int j=0;j<n;j++){
+            dfs(height,CanReachPacific,0,j);
+            dfs(height,CanReachAtlantic,m-1,j);
         }
-        for (int i = 0; i < m; i++) {
-            dfs(height, pacific, i, 0);
-            dfs(height, atlantic, i, n - 1);
+        for(int i=0;i<m;i++){
+            dfs(height,CanReachPacific,i,0);
+            dfs(height,CanReachAtlantic,i,n-1);
         }
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (pacific[i][j] && atlantic[i][j]) {
-                    List<Integer> coordinate = new ArrayList<>();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(CanReachPacific[i][j] && CanReachAtlantic[i][j]){
+                    List<Integer> coordinate=new ArrayList<>();
                     coordinate.add(i);
                     coordinate.add(j);
                     result.add(coordinate);
@@ -31,20 +29,17 @@ class Solution {
         return result;
     }
 
-    private void dfs(int[][] height, boolean[][] canReachOcean, int i, int j) {
-        int m = height.length;
-        int n = height[0].length;
-        canReachOcean[i][j] = true;
+    private void dfs(int[][] height, boolean[][] reachOcean, int row, int col) {
+        reachOcean[row][col] = true;
+        int[][] direction = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+        for (int[] dir : direction) {
+            int i = row + dir[0];
+            int j = col + dir[1];
 
-        int[][] directions = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
-        for (int[] dir : directions){
-            int newRow = i + dir[0];
-            int newCol = j + dir[1];
-
-            if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && !canReachOcean[newRow][newCol] &&
-               height[newRow][newCol] >= height[i][j]) {
-                dfs(height, canReachOcean, newRow, newCol);
+            if (i >= 0 && i < height.length && j >= 0 && j < height[0].length && !reachOcean[i][j] && height[row][col] <= height[i][j]) {
+                dfs(height, reachOcean, i, j);
             }
         }
+
     }
 }
