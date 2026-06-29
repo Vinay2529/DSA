96f51ba3d86ad435603ada149355bad7429caedc
Vinay2529 <123531815+Vinay2529@users.noreply.github.com>
2026-06-29 06:21:39 +0530

    Time: 3 ms (87.45%) | Memory: 52.3 MB (46.10%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,32 +1,29 @@
 class Solution {
     public int numIslands(char[][] grid) {
         int count=0;
-        int row=grid.length,col=grid[0].length;
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j]=='1'){
                     count++;
-                    dfs(i,j,grid);
+                    dfs(grid,i,j);
                 }
             }
         }
         return count;
     }
-    private void dfs(int row,int col,char[][] grid){
-        if(isValid(row,col,grid)){
-            grid[row][col]='2';
-            dfs(row+1,col,grid);
-            dfs(row-1,col,grid);
-            dfs(row,col+1,grid);
-            dfs(row,col-1,grid);
+    private void dfs(char[][] grid,int row,int col){
+        if(isValid(grid,row,col)){
+            grid[row][col]=2;
+            dfs(grid,row-1,col);
+            dfs(grid,row+1,col);
+            dfs(grid,row,col+1);
+            dfs(grid,row,col-1);
         }
     }
-
-    private boolean isValid(int row,int col,char[][] grid){
-        if(row<0 || row>=grid.length) return false;
-        if(col<0 || col>=grid[0].length) return false;
-
-        if(grid[row][col]=='1') return true;
+    private boolean isValid(char[][] grid,int row,int col){
+        if(row>=0 && row<grid.length && col>=0 && col<grid[0].length && grid[row][col]=='1'){
+            return true;
+        }
         return false;
     }
 }
