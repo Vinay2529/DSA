274a668ef7ae981765a588b2792bd4d6ddac45b5
Vinay2529 <123531815+Vinay2529@users.noreply.github.com>
2026-08-21 13:09:32 +0530

    Time: 104 ms (39.65%) | Memory: 47.5 MB (5.63%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,10 +1,17 @@
 class Solution {
+    private HashMap<String,Integer> mp;
     public int findTargetSumWays(int[] nums, int target) {
+        mp=new HashMap<>();
         return f(0,target,nums);
     }
     public int f(int i,int target,int[] nums){
+        String key=i+"-"+target;
+        if(mp.containsKey(key)){
+            return mp.get(key);
+        }
         if(i<nums.length-1){
-            return f(i+1,target-nums[i],nums)+f(i+1,target+nums[i],nums);
+           mp.put(key,f(i+1,target-nums[i],nums)+f(i+1,target+nums[i],nums));
+           return mp.get(key);
         }
         else{
             if(Math.abs(target)==Math.abs(nums[i])){
