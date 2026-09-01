203fdfe318ae678a9b5c8deeba6461d95e257045
Vinay2529 <123531815+Vinay2529@users.noreply.github.com>
2026-09-01 13:49:32 +0530

    Time: 17 ms (49.81%) | Memory: 46.6 MB (41.09%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,22 +1,22 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int[] dp=new int[amount+1];
+        Arrays.fill(dp,-1);
         dp[0]=0;
-
         for(int coin:coins){
             if(coin<=amount){
                 dp[coin]=1;
             }
         }
-        for(int x=1;x<=amount;x++){
+        for(int i=1;i<=amount;i++){
             int ans=-1;
             for(int coin:coins){
-                if(x<coin) continue;
-                if(dp[x-coin]!=-1 &&(ans==-1 || dp[x-coin]+1<ans)){
-                    ans=dp[x-coin]+1;
+                if(i<coin)continue;
+                if(dp[i-coin]!=-1 && (ans==-1 || dp[i-coin]+1<ans)){
+                    ans=dp[i-coin]+1;
                 }
             }
-            dp[x]=ans;
+            dp[i]=ans;
         }
         return dp[amount];
     }
