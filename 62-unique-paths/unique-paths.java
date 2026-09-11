7881087d19f98a6d3b70050d139efccef18ebfb0
Vinay2529 <123531815+Vinay2529@users.noreply.github.com>
2026-09-11 09:28:21 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (81.81%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,14 +1,25 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int totalMoves = m + n - 2;
-        int moves = Math.min(m - 1, n - 1);
-
-        long result = 1;
-
-        for (int i = 1; i <= moves; i++) {
-            result = result * (totalMoves - moves + i) / i;
+        int[][] dp=new int[m][n];
+        for(int[] row:dp){
+            Arrays.fill(row,-1);
         }
+        dp[m-1][n-1]=1;
+        return f(m,n,0,0,dp);
 
-        return (int) result;
+    }
+    private int f(int m,int n,int i,int j,int[][] dp){
+        if(i<0 || i>=m || j<0 || j>=n){
+            return 0;
+        }else
+        {
+            if(dp[i][j]!=-1){
+                return dp[i][j];
+            }
+            else{
+                dp[i][j]=f(m,n,i+1,j,dp) +f(m,n,i,j+1,dp);
+            }
+            return dp[i][j];
+        }
     }
 }
